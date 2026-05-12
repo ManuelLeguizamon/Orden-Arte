dc66774ce2576bf75e43140571184ddb999e7a72
Manuel Leguizamon <leguimanu@hotmail.com>
2026-05-12 15:15:32 -0300

    Actualizados requerimientos para login de Google

--- apps/usuario/urls.py
+++ apps/usuario/urls.py
@@ -3,6 +3,6 @@ from apps.usuario.views import UsuarioView, LoginView, SignUpView, logout_view
 urlpatterns = [
     path('', UsuarioView.as_view(), name="usuario"),
     path('login/', LoginView.as_view(), name="login"),
-    path('signup/', SignUpView.as_view(), name='signup'),
+    # path('signup/', SignUpView.as_view(), name='signup'),
     path('logout/', logout_view, name='logout'),
     ]
